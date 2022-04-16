601dce6ea3aa4eb1b5369a4bee48ec38e37a7759
fardeenkhan7337 <fardeenkhan7337@gmail.com>
2022-04-17 03:18:46 +0500

    dashboard design error solved

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -2,7 +2,7 @@
 <div class="iq-navbar-header" style="height: 215px;">
   <div class="container-fluid iq-container">
     <div class="row">
-      <div class="col-md-12">
+      <div class="col-md-12" style="z-index:1!important;">
         <div class="flex-wrap d-flex justify-content-between align-items-center">
           <div>
             <h1>Hello John!</h1>
@@ -11,24 +11,14 @@
       </div>
     </div>
   </div>
-  <div class="iq-header-img">
+  <div class="iq-header-img" style="z-index:0!important;">
     <img src="<?= base_url('assets/images/dashboard/top-header.png') ?>" alt="header"
       class="theme-color-default-img img-fluid w-100 h-100 animated-scaleX">
-    <img src="<?= base_url('assets/images/dashboard/top-header1.png') ?>" alt="header"
-      class="theme-color-purple-img img-fluid w-100 h-100 animated-scaleX">
-    <img src="<?= base_url('assets/images/dashboard/top-header2.png') ?>" alt="header"
-      class="theme-color-blue-img img-fluid w-100 h-100 animated-scaleX">
-    <img src="<?= base_url('assets/images/dashboard/top-header3.png') ?>" alt="header"
-      class="theme-color-green-img img-fluid w-100 h-100 animated-scaleX">
-    <img src="<?= base_url('assets/images/dashboard/top-header4.png') ?>" alt="header"
-      class="theme-color-yellow-img img-fluid w-100 h-100 animated-scaleX">
-    <img src="<?= base_url('assets/images/dashboard/top-header5.png') ?>" alt="header"
-      class="theme-color-pink-img img-fluid w-100 h-100 animated-scaleX">
   </div>
 </div> <!-- Nav Header Component End -->
 <!--Nav End-->
 </div>
-<div class="conatiner-fluid content-inner mt-n5 py-0">
+<div class="conatiner-fluid content-inner mt-n5 py-0" style=" padding-top: 0px!important;background: #e9ecef !important;">
 <div class="row">
   <div class="col-md-12 col-lg-12">
     <div class="row row-cols-1">
